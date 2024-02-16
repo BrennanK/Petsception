38651303418bdb586226de20890768f39af76d96
BrennanK <bksafari17@gmail.com>
2024-02-16 14:47:50 -0600

    Added framework

--- Petsception/Assets/Scripts/Game_Manager.cs
+++ Petsception/Assets/Scripts/Game_Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Game_Manager : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class Game_Manager : MonoBehaviour
 
     [SerializeField]
     private TMP_Text timerText;
+
+    [SerializeField]
+    private List<Pet> pets;
 
     // Start is called before the first frame update
     void Awake()
@@ -24,6 +28,7 @@ public class Game_Manager : MonoBehaviour
         {
             instance = this;
         }
+       
     }
 
     // Update is called once per frame
@@ -37,6 +42,13 @@ public class Game_Manager : MonoBehaviour
     {
         int seconds = ((int)timeToSolveInSeconds % 60);
         int minutes = ((int)timeToSolveInSeconds / 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        if(seconds==0 && minutes==0)
+        {
+            //SceneManager.LoadScene(0);
+        }
     }
+
+    
 }
