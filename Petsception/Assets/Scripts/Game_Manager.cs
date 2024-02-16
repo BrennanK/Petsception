c4900fb4c974ee14af987a33414bebf258460f25
BrennanK <bksafari17@gmail.com>
2024-02-16 11:48:29 -0600

    Adding TextMeshPro and working on Timer

---
+++ Petsception/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager instance {get; private set;}
+
+    [SerializeField]
+    float timeToSolveInSeconds;
+
+    [SerializeField]
+    private TMP_Text timerText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance!=null && instance!=this)
+        {
+            Destroy(this);
+        }
+        else 
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToSolveInSeconds -= Time.deltaTime;
+        updateTimer();
+    }
+
+    private void updateTimer()
+    {
+        int seconds = ((int)timeToSolveInSeconds % 60);
+        int minutes = ((int)timeToSolveInSeconds / 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
